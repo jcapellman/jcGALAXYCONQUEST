3e74b2c8096fe63a1256ccdf259a0d5d57467a35
Jarred Capellman <jcapellman@hotmail.com>
2016-03-26 16:16:21 -0700

    Initial commit of the code

---
+++ jcGALAXYCONQUEST.UWP/Views/DetailPage.xaml.cs
@@ -0,0 +1,12 @@
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace jcGALAXYCONQUEST.UWP.Views {
+    public sealed partial class DetailPage : Page {
+        public DetailPage() {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
---
+++ jcGALAXYCONQUEST.UWP/Views/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+using Windows.UI.Xaml.Controls;
+
+namespace jcGALAXYCONQUEST.UWP.Views {
+    public sealed partial class MainPage : Page {
+        public MainPage() {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
