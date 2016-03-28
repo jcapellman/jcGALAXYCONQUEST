2aec473b9e3ac27444d19ebc565b4b9a55cc673e
Jarred Capellman <jcapellman@hotmail.com>
2016-03-28 08:40:52 -0700

    Initial work on the Load Game UI

---
+++ jcGALAXYCONQUEST.UWP/Views/LoadGamePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+using jcGALAXYCONQUEST.UWP.ViewModels;
+
+namespace jcGALAXYCONQUEST.UWP.Views {
+    public sealed partial class LoadGamePage : Page {
+        private LoadGameViewModel viewModel => (LoadGameViewModel) DataContext;
+
+        public LoadGamePage() {
+            this.InitializeComponent();
+
+            DataContext = new LoadGameViewModel();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e) {
+            var result = await viewModel.LoadData();
+        }
+    }
+}
